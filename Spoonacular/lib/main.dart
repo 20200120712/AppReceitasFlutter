479c217cc0a21c0a1a98875e4a6af251f7c17396
20200120712 <112270865+20200120712@users.noreply.github.com>
2023-06-02 22:49:11 +0000

    continuação

--- Spoonacular/lib/main.dart
+++ Spoonacular/lib/main.dart
@@ -83,4 +83,39 @@ class _PesquisarReceitasPageState extends State<PesquisarReceitasPage> {
 }
 
 class DescreverReceitasPage extends StatelessWidget {
-/////////////////////////////////////////////////////
+  final Map<String, dynamic> recipeDetails;
+
+  DescreverReceitasPage(this.recipeDetails);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(recipeDetails['title']),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Image.network(recipeDetails['image']),
+            SizedBox(height: 16),
+            Text(
+              'Ingredientes:',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 8),
+            Expanded(
+              child: ListView.builder(
+                itemCount: recipeDetails['extendedIngredientes'].length,
+                itemBuilder: (context, index) {
+                  return Text('- ${recipeDetails['extendedIngredientes'][index]['original']}');
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
